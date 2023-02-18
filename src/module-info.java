aa613cf135639d82edb0c9e27a70dcd3982f656c
kshashi183 <shashidhar.k@ahana.co.in>
2023-02-18 22:32:02 +0530

    First commit..

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Shashidhar
+ *
+ */
+module GitDemoProjectV1 {
+}
---
+++ src/packg/Test1.java
@@ -0,0 +1,10 @@
+package packg;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
